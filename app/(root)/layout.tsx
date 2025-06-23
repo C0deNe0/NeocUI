69a6c4cf0bb7cd8034f249f032aaba39b3104b32
C0deNe0 <access.naveenhiremath@gmail.com>
2025-06-23 15:23:42 +0530

    testing prittier

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,19 +4,23 @@ import { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: {
-    template: 'NeoC UI = openSouce Components',
-    default: 'NeoC UI',
-  },
+    title: {
+        template: 'NeoC UI = openSouce Components',
+        default: 'NeoC UI',
+    },
 };
 
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Header />
-      <main className="relative w-full pt-0 md:pt-0">{children}</main>
+export default function HomeLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <Header />
+            <main className="relative w-full pt-0 md:pt-0">{children}</main>
 
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Hello there from naveen </h1>;
+    return <h1>Hello there from naveen </h1>;
 }
